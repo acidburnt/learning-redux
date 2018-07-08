45043be1804406c9635eb946f3ffe69bf1dd1b1a
Michal Szydlowski <michal.szydlowski@netguru.co>
2018-07-09 00:58:30 +0200

    Lesson 12

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -42,6 +42,6 @@ export const getVisibleTodos = (state, filter) => {
     case 'active':
       return allTodos.filter(t => !t.completed);
     default:
-      throw new Error(`Unknown filter ${filter}`);
+      return allTodos;
   }
 };
